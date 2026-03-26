e8cc671a35f81e4b6e7f39bc729e9b0a3e6ab89f
Robin Nash <84207317+RobinNash@users.noreply.github.com>
2026-03-26 19:20:41 -0400

    Update lambda_function1.py

--- challenges/easy/lambda_function1.py
+++ challenges/easy/lambda_function1.py
@@ -1,7 +1,7 @@
 """
 Simple Temperature Converter Lambda Function
 Convert Celsius to Fahrenheit
-    
+     
 Expected input: {"temperature": 25}
 Expected output: {"statusCode": 200, "body": 77}
 """
